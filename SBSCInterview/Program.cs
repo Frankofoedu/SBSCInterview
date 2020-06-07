0fe593333ae3e6273084dc77e6f43babb4fd5ce7
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-06-07 11:50:53 +0100

    added string occurence code

--- SBSCInterview/Program.cs
+++ SBSCInterview/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SBSCInterview
 {
@@ -6,10 +8,45 @@ namespace SBSCInterview
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine(StringOccurrence(""));
+
+            Console.Read();
         }
 
 
-        static void 
+        static string StringOccurrence(string inputString)
+        {
+            //loop through string
+            //for each element, check if it exists in the dictionary
+            //if it exists, increment count else add to the dictionary
+
+            if (string.IsNullOrEmpty(inputString))
+            {
+                return "Empty string provided";
+            }
+
+            var dictOccurence = new Dictionary<char, int>();
+            var sb = new StringBuilder();
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                var c = inputString[i];
+
+                if (!dictOccurence.ContainsKey(c))
+                {
+                    dictOccurence.Add(c, 1);
+                }
+                else
+                {
+                    dictOccurence[c]++;
+                }
+            }
+
+            foreach (var item in dictOccurence)
+            {
+                sb.Append($"{item.Key}{item.Value}");
+            }
+
+            return sb.ToString(); ;
+        }
     }
 }
