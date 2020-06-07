13781ecf77e25cc6d500d105d5935a57ae9e0472
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-06-07 10:58:17 +0100

    Add project files.

---
+++ SBSCInterview/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SBSCInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        static void 
+    }
+}
